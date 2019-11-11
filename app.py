cc38f66ab0ab258e0ccf370ad5d78014a4398187
JohnTagliaferro <john.tagliaferro@tufts.edu>
2019-11-11 17:28:37 -0500

    integrated with Jedi_academy image processing moduel to generate heatmaps

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, escape, request, jsonify, render_template, send_file
 from werkzeug.utils import secure_filename
 import os
+from mnist_cnn import get_info
 
 UPLOAD_FOLDER = 'uploaded_images';
 ALLOWED_EXTENSIONS = {'png'}
@@ -19,7 +20,7 @@ def is_word_data(data):
     return data == '2'
 
 def pic_model_1(data):
-    return "It's a cat"
+    return get_info(data)
 
 def word_model_1(data):
     return "The house should cost one million dollars"
@@ -36,6 +37,10 @@ models = {
 def hello():
     #name = request.args.get("name", "World")
     return render_template('index.html')
+
+# TODO Make this random
+def random_thing():
+    return "128"
 
 @app.route('/mlmodel', methods = ['POST', 'GET'])
 def mlmodel():
@@ -56,8 +61,10 @@ def mlmodel():
             return '', 401
         if image_file and allowed_file(image_file.filename):
             filename = secure_filename(image_file.filename)
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], random_thing() + filename)
             image_file.save(filepath)
-            return send_file(filepath)
+            ret_file = model_func(filepath)
+            print(ret_file)
+            return send_file(ret_file)
         return '', 402
     return model_func(test_data), 200
---
+++ mnist_cnn.py
@@ -0,0 +1,47 @@
+import image_processing as ip
+import numpy as np
+from PIL import Image
+
+def gen_heatmap(model, image, filename, adv=False):
+    target = np.argmax(model.predict(image.reshape(1, 28, 28, 1)))
+    print(target)
+    misses = np.zeros((28,28), dtype="float32")
+    gen = ip.GrayscalePerturbator(image, grid_dimen=1, stride=1, stride_scale=1, cutoff_dimen=28, is_adversarial=adv)
+    for pert in gen:
+        label = np.argmax(model.predict(pert.reshape(1, 28, 28, 1)))
+        if ((not adv) and label == target) or (adv and label != target):
+            indices = gen.maskedIndices
+            for index in indices:
+                r, c, grid_dimen = index
+                misses[r][c] += 1.0 / (grid_dimen ** 2)
+        else:
+            pass
+    
+    maxMiss = np.max(misses)
+    if maxMiss != 0:
+        scaled = np.array(misses * (255.0 / maxMiss), dtype="uint8")
+    else:
+        scaled = np.array(misses, dtype="uint8")
+
+    i = Image.fromarray(scaled, mode="L")
+    i.save(filename)
+    return filename
+
+
+
+def get_info(image_file):
+    image = np.array(Image.open(image_file))
+    model = ip.get_pretrained_mnist_cnn()
+    return gen_heatmap(model, image, image_file + "heatmap.png")
+
+
+#filename = "file.png"
+#mnister(filename)
+"""
+
+from keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x= x_train[1]
+i = Image.fromarray(x, mode="L")
+i.save("1.png")
+"""
